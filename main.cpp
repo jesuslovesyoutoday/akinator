c9e7405734632e6c3bb0e3e64970af60ac765255
jesuslovesyoutoday <jesuslovesyoutoday@yadex.ru>
2021-11-15 21:54:55 +0300

    added a cat

--- main.cpp
+++ main.cpp
@@ -6,6 +6,35 @@ int main()
     char file[100] = "base.txt";
     struct tree tree;
     treeCtor(&tree, file);
+    
+    puts("=============================================");
+    puts("            Welcome to akinator!");
+    puts("=============================================\n");
+    
+    puts("                        *                    ");
+    puts("                       /\\                    ");
+    puts("                  /\\__/__\\_/\\                ");
+    puts("                 / ===   === \\               ");
+    puts("                |  (#) _ (#)  |              ");
+    puts("                |     / \\     |              ");
+    puts("                 \\           /               ");
+    puts("                  \\__      _/                ");
+    puts("                    /     /                  ");
+    puts("                   |   __/                   ");
+    puts("                    | /                      ");
+    puts("                     |\\                      ");
+    puts("                     |/                      ");
+    puts("                   __|__                     ");
+    puts("                   \\_ _/                     ");
+    puts("                    |#|                      ");
+    puts("                   |###|                     ");
+    puts("                  |#####|                    ");
+    puts("                  |#####|                    ");
+    puts("                   -----                     ");
+    
+    puts("                Choose mode:\n");
+    puts("  guessing | defining | comparing | show_base\n");   
+    
     treeDtor(&tree);
     
     return 0;
